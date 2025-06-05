efa982543fa25aee8727837f49bfb26f7d1f2765
Asive05 <danielasivesimoki05@gmail.com.com>
2025-06-05 11:33:31 +0200

    password reset page - Asive

--- src/components/ResetPassword.vue
+++ src/components/ResetPassword.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="reset-password">
-    <h2>Reset Password</h2>
+    <img src="/logo 1.png" alt="App Logo">  <!-- new added code line -->
+    <h2>Password Reset </h2>
     <form @submit.prevent="handleSubmit">
-      <label for="password">New Password:</label>
-      <input type="password" v-model="newPassword" required />
+        <p class="inspirational-text">
+    Youth is 37% of South Africa... but 100% of its future
+  </p> <!-- new added code line -->
 
-      <button type="submit" :disabled="loading">
-        {{ loading ? 'Resetting...' : 'Reset Password' }}
+  <label for="email"></label>
+    <input 
+      type="email" 
+      id="email" 
+      v-model="email"  
+      placeholder="📧    Email"  
+
+      required
+    >
+      <button type="submit" :disabled="loading" class="button">
+        {{ loading ? 'Resetting...' : 'Submit' }}
       </button>
     </form>
 
@@ -52,12 +63,18 @@ export default {
 <style scoped>
 .reset-password {
   max-width: 400px;
-  margin: auto;
-  padding: 20px;
+  margin: 80px auto 0;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.08);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  height: 400px;
 }
+
 .message {
   color: green;
-  margin-top: 10px;
+  margin-top: 10px; 
 }
 .error {
   color: red;
@@ -67,4 +84,48 @@ button[disabled] {
   opacity: 0.6;
   cursor: not-allowed;
 }
+
+
+
+.inspirational-text{
+  color : grey
+}
+.reset-password > img {
+  align-self: flex-start;  /* align logo to left */
+}
+
+.button {
+  width: 40%;
+  background-color: #003c8f;
+  color: rgba(255, 255, 255, 0.753);
+  border: none;
+  border-radius: 12px;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #002f6c;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  gap: 2rem;
+  width: 100%;
+}
+
+input[type="email"] {
+  padding: 15px;
+  margin-top : -55px;
+  border: 1px solid #ccc;
+  font-size: 1.1rem;
+  width: 80%;
+}
+
+label {
+  align-self: flex-start;
+}
+
 </style>
